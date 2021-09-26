994306445647fb9a043fe2d0694b252ff02bf079
souvik1914581 <souvikroyofficial10@gmail.com>
2021-09-26 18:54:06 +0530

    Added id3v1 Tag Read

--- Source/mp3TagEditor.cpp
+++ Source/mp3TagEditor.cpp
@@ -28,6 +28,19 @@
 		
 		debugMsg = "File Size";
 		debugPrint1(debugMsg,m_fileSize);
+		
+		/*goto 128 bytes from end of file and read 3 bytes*/
+		m_fileStream.seekg(m_fileSize - 128,std::ios::beg);
+		
+		debugMsg = "Current pos";
+		debugPrint1(debugMsg,m_fileStream.tellg());
+		
+		
+		char *buff = new char[3];
+		m_fileStream.read(buff,3);
+		
+		debugMsg = "Bytes read = " + std::string(buff);
+		debugPrint(debugMsg);
 	}
 	
 	mp3File :: ~mp3File()
